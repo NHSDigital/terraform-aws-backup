7d29598a3a54a5ffd1531ebff78cc59767f75516
regularfry <alex.young12@nhs.net>
2024-09-18 14:34:14 +0100

    Add comments to the destination variables

--- modules/destination/variables.tf
+++ modules/destination/variables.tf
@@ -1,14 +1,21 @@
 variable "source_account_name" {
+  # This is used as a prefix for the vault name, and referenced by the policy and the lock.
+  # It doesn't have to match anything in the source AWS account.
   description = "The name of the account that backups will come from"
   type        = string
 }
 
 variable "source_account_id" {
+  # The source account ID is used in the policy to allow permit root in the source account
+  # to copy backups into the vault.
   description = "The id of the account that backups will come from"
   type        = string
 }
 
 variable "account_id" {
+  # This is used to deny root from being able to copy backups from the vault
+  # to anywhere other than the source account.  The constraint will need to
+  # be removed if the original source account is lost.
   description = "The id of the account that the vault will be in"
   type        = string
 }
@@ -25,6 +32,8 @@ variable "kms_key" {
 }
 
 variable "enable_vault_protection" {
+  # With this set to true, privileges are locked down so that the vault can't be deleted or
+  # have its policy changed. The minimum and maximum retention periods are also set only if this is true.
   description = "Flag which controls if the vault lock is enabled"
   type        = bool
   default     = false
@@ -35,6 +44,7 @@ variable "vault_lock_type" {
   type        = string
   # See toplevel README.md:
   #   DO NOT SET THIS TO compliance UNTIL YOU ARE SURE THAT YOU WANT TO LOCK THE VAULT PERMANENTLY
+  # When you do, you will also need to set "enable_vault_protection" to true for it to take effect.
   default     = "governance"
 }
 
