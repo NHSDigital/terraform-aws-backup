0f45e3777b286cb0458808b6eede462e07ccb762
Gavin Dearden <gavin.dearden2@nhs.net>
2025-05-13 20:32:00 +0100

    Add the missing default for selection_tags

--- modules/aws-backup-source/locals.tf
+++ modules/aws-backup-source/locals.tf
@@ -1,5 +1,5 @@
 locals {
   resource_name_prefix = "${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}-backup"
-  selection_tags_null_adjusted = (var.backup_plan_config.selection_tags == []) ? [{ "key": "${var.backup_plan_config.selection_tag}" , "value": "${var.backup_plan_config.selection_tag_value}" }] : var.backup_plan_config.selection_tags
-  selection_tags_dynamodb_null_adjusted = (var.backup_plan_config_dynamodb.selection_tags == []) ? [{ "key": "${var.backup_plan_config_dynamodb.selection_tag}", "value":"${var.backup_plan_config_dynamodb.selection_tag_value}" }] : var.backup_plan_config_dynamodb.selection_tags
+  selection_tags_null_adjusted = (var.backup_plan_config.selection_tags == null) ? [{ "key": "${var.backup_plan_config.selection_tag}" , "value": "${var.backup_plan_config.selection_tag_value}" }] : var.backup_plan_config.selection_tags
+  selection_tags_dynamodb_null_adjusted = (var.backup_plan_config_dynamodb.selection_tags == null) ? [{ "key": "${var.backup_plan_config_dynamodb.selection_tag}", "value":"${var.backup_plan_config_dynamodb.selection_tag_value}" }] : var.backup_plan_config_dynamodb.selection_tags
 }
