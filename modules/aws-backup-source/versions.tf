e0bfcdf6db813f9551d9f458c49f52b839bb483f
Alastair Montgomery <alastair.montgomery1@nhs.net>
2025-05-13 17:00:05 +0100

    SPINECORE-6193 provider versions for TFLint

---
+++ modules/aws-backup-source/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 1"
+    }    
+  }
+
+  required_version = ">= 1.9.5"
+}
