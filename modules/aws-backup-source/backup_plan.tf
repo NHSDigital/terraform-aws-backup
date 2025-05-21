798f8ce0ff7d213ab99848662d416e420785df16
aidanharrisnhs <aidan.harris3@nhs.net>
2025-05-21 16:30:10 +0100

    added event bridge lambda and new vault to facilitate new cross account copy job method

--- modules/aws-backup-source/backup_plan.tf
+++ modules/aws-backup-source/backup_plan.tf
@@ -91,6 +91,36 @@ resource "aws_backup_plan" "ebsvol" {
   }
 }
 
+resource "aws_backup_plan" "rds" {
+  count = var.backup_plan_config_rds.enable ? 1 : 0
+  name  = "${local.resource_name_prefix}-rds-plan"
+
+  dynamic "rule" {
+    for_each = var.backup_plan_config_rds.rules
+    content {
+      recovery_point_tags = {
+        backup_rule_name = rule.value.name
+      }
+      rule_name         = rule.value.name
+      target_vault_name = aws_backup_vault.main.name
+      schedule          = rule.value.schedule
+      lifecycle {
+        delete_after       = rule.value.lifecycle.delete_after != null ? rule.value.lifecycle.delete_after : null
+        cold_storage_after = rule.value.lifecycle.cold_storage_after != null ? rule.value.lifecycle.cold_storage_after : null
+      }
+      dynamic "copy_action" {
+        for_each = rule.value.copy_action != null ? rule.value.copy_action : {}
+        content {
+          lifecycle {
+            delete_after = copy_action.value
+          }
+          destination_vault_arn = aws_backup_vault.intermediary-vault[0].arn
+        }
+      }
+    }
+  }
+}
+
 resource "aws_backup_selection" "default" {
   iam_role_arn = aws_iam_role.backup.arn
   name         = "${local.resource_name_prefix}-selection"
@@ -148,6 +178,28 @@ resource "aws_backup_selection" "ebsvol" {
   condition {
     dynamic "string_equals" {
       for_each = local.selection_tags_ebsvol_null_checked
+      content {
+        key   = (try(string_equals.value.key, null) == null) ? null : "aws:ResourceTag/${string_equals.value.key}"
+        value = try(string_equals.value.value, null)
+      }
+    }
+  }
+}
+
+resource "aws_backup_selection" "rds" {
+  count        = var.backup_plan_config_rds.enable ? 1 : 0
+  iam_role_arn = aws_iam_role.backup.arn
+  name         = "${local.resource_name_prefix}-rds-selection"
+  plan_id      = aws_backup_plan.rds[0].id
+
+  selection_tag {
+    key   = var.backup_plan_config_rds.selection_tag
+    type  = "STRINGEQUALS"
+    value = (var.backup_plan_config_rds.selection_tag_value == null) ? "True" : var.backup_plan_config_rds.selection_tag_value
+  }
+  condition {
+    dynamic "string_equals" {
+      for_each = local.selection_tags_rds_null_checked
       content {
         key   = (try(string_equals.value.key, null) == null) ? null : "aws:ResourceTag/${string_equals.value.key}"
         value = try(string_equals.value.value, null)
---
+++ modules/aws-backup-source/lambda_copy_job.tf
@@ -0,0 +1,80 @@
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda_copy_job" {
+  count       = var.backup_plan_config_rds.enable ? 1 : 0
+  name               = "iam_for_cross_account_copy_job_lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+data "aws_iam_policy_document" "lambda_copy_job_permissions" {
+  version = "2012-10-17"
+  statement {
+    effect    = "Allow"
+    actions   = ["kms:Decrypt", "kms:GenerateDataKey"]
+    resources = [aws_kms_key.aws_backup_key.arn]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "backup:StartCopyJob",
+      "backup:DescribeRecoveryPoint",
+      "backup:ListRecoveryPointsByBackupVault"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = [aws_iam_role.backup.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "cross_account_iam_permissions" {
+  count       = var.backup_plan_config_rds.enable ? 1 : 0
+  name   = "cross_account_iam_permissions_policy"
+  role   = aws_iam_role.iam_for_lambda_copy_job[0].id
+  policy = data.aws_iam_policy_document.lambda_copy_job_permissions.json
+}
+
+data "archive_file" "start_cross_account_copy_job_lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/resources"
+  output_path = "${path.module}/.terraform/archive_files/start_cross_account_copy_job_lambda.zip"
+}
+
+resource "aws_lambda_function" "start_cross_account_copy_job_lambda" {
+  count       = var.backup_plan_config_rds.enable ? 1 : 0
+  filename         = data.archive_file.start_cross_account_copy_job_lambda_zip.output_path
+  source_code_hash = data.archive_file.start_cross_account_copy_job_lambda_zip.output_base64sha256
+  function_name    = "start_cross_account_copy_job"
+  role             = aws_iam_role.iam_for_lambda_copy_job[0].arn
+  handler          = "start_cross_account_copy_job.lambda_handler"
+  runtime          = "python3.12"
+  environment {
+    variables = {
+      aws_account_id                     = data.aws_caller_identity.current.account_id,
+      backup_account_id                  = var.backup_copy_vault_account_id,
+      backup_copy_vault_arn              = var.backup_copy_vault_arn,
+      backup_role_arn                    = aws_iam_role.backup.arn,
+      destination_vault_retention_period = var.destination_vault_retention_period
+    }
+  }
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  count       = var.backup_plan_config_rds.enable ? 1 : 0
+  statement_id  = "AllowExecutionFromEventbridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.start_cross_account_copy_job_lambda[0].function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = "arn:aws:events:eu-west-2:${data.aws_caller_identity.current.account_id}:rule/Cross-Account-Copy-Job-rule"
+}
