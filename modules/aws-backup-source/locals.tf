af2d87b5d1078c98944f3b5134b4921f0a15e0b4
aidanharrisnhs <aidan.harris3@nhs.net>
2025-06-20 13:47:57 +0100

    event bridge and lambda implementation

--- modules/aws-backup-source/locals.tf
+++ modules/aws-backup-source/locals.tf
@@ -1,8 +1,10 @@
 locals {
   selection_tag_value_null_checked          = (var.backup_plan_config.selection_tag_value == null) ? "True" : var.backup_plan_config.selection_tag_value
   selection_tag_value_dynamodb_null_checked = (var.backup_plan_config_dynamodb.selection_tag_value == null) ? "True" : var.backup_plan_config_dynamodb.selection_tag_value
+  selection_tag_value_rds_null_checked      = (var.backup_plan_config_rds.selection_tag_value == null) ? "True" : var.backup_plan_config_rds.selection_tag_value
   selection_tags_null_checked               = (var.backup_plan_config.selection_tags == null) ? [{ "key" : var.backup_plan_config.selection_tag, "value" : local.selection_tag_value_null_checked }] : var.backup_plan_config.selection_tags
   selection_tags_dynamodb_null_checked      = (var.backup_plan_config_dynamodb.selection_tags == null) ? [{ "key" : var.backup_plan_config_dynamodb.selection_tag, "value" : local.selection_tag_value_dynamodb_null_checked }] : var.backup_plan_config_dynamodb.selection_tags
+  selection_tags_rds_null_checked           = (var.backup_plan_config_rds.selection_tags == null) ? [{ "key" : var.backup_plan_config_rds.selection_tag, "value" : local.selection_tag_value_rds_null_checked }] : var.backup_plan_config_rds.selection_tags
   selection_tag_value_ebsvol_null_checked   = (var.backup_plan_config_ebsvol.selection_tag_value == null) ? "True" : var.backup_plan_config_ebsvol.selection_tag_value
   selection_tags_ebsvol_null_checked        = (var.backup_plan_config_ebsvol.selection_tags == null) ? [{ "key" : var.backup_plan_config_ebsvol.selection_tag, "value" : local.selection_tag_value_ebsvol_null_checked }] : var.backup_plan_config_ebsvol.selection_tags
   framework_arn_list = flatten(concat(
--- modules/aws-backup-source/variables.tf
+++ modules/aws-backup-source/variables.tf
@@ -85,6 +85,12 @@ variable "backup_copy_vault_account_id" {
   default     = ""
 }
 
+variable "destination_vault_retention_period" {
+  description = "Retention period for recovery points made with the copy job lambda"
+  type        = number
+  default     = 365
+}
+
 variable "backup_plan_config" {
   description = "Configuration for backup plans"
   type = object({
@@ -215,6 +221,72 @@ variable "backup_plan_config_dynamodb" {
       },
       {
         name     = "dynamodb_monthly_kept_7_years"
+        schedule = "cron(0 2 1  * ? *)"
+        lifecycle = {
+          cold_storage_after = 30
+          delete_after       = 2555
+        }
+        copy_action = {
+          delete_after = 365
+        }
+      }
+    ]
+  }
+}
+
+variable "backup_plan_config_rds" {
+  description = "Configuration for backup plans with rds"
+  type = object({
+    enable              = bool
+    selection_tag       = string
+    selection_tag_value = optional(string)
+    selection_tags = optional(list(object({
+      key   = optional(string)
+      value = optional(string)
+    })))
+    compliance_resource_types = list(string)
+    rules = optional(list(object({
+      name                     = string
+      schedule                 = string
+      enable_continuous_backup = optional(bool)
+      lifecycle = object({
+        delete_after       = number
+        cold_storage_after = optional(number)
+      })
+      copy_action = optional(object({
+        delete_after = optional(number)
+      }))
+    })))
+  })
+  default = {
+    enable                    = true
+    selection_tag             = "BackupRDS"
+    selection_tag_value       = "True"
+    selection_tags            = []
+    compliance_resource_types = ["RDS"]
+    rules = [
+      {
+        name     = "rds_daily_kept_5_weeks"
+        schedule = "cron(0 0 * * ? *)"
+        lifecycle = {
+          delete_after = 35
+        }
+        copy_action = {
+          delete_after = 365
+        }
+      },
+      {
+        name     = "rds_weekly_kept_3_months"
+        schedule = "cron(0 1 ? * SUN *)"
+        lifecycle = {
+          delete_after = 90
+        }
+        copy_action = {
+          delete_after = 365
+        }
+      },
+      {
+        name     = "rds_monthly_kept_7_years"
         schedule = "cron(0 2 1  * ? *)"
         lifecycle = {
           cold_storage_after = 30
