f4f3c4c571b2955d7b435b86299a73e46e69d7ec
staplesn <nicholas.staples@solution-tribute.com>
2025-10-19 21:11:52 +0100

    Add enable functionality for parameter_store module.

--- modules/aws-backup-source/lambda_parameter_store_backup.tf
+++ modules/aws-backup-source/lambda_parameter_store_backup.tf
@@ -1,4 +1,5 @@
 data "aws_iam_policy_document" "lambda_assume_role" {
+  count = var.backup_plan_config_parameter_store.enable ? 1 : 0
   statement {
     effect = "Allow"
     principals {
@@ -9,35 +10,15 @@ data "aws_iam_policy_document" "lambda_assume_role" {
   }
 }
 
-resource "aws_s3_bucket" "parameter_store_backup_storage" {
-  bucket = "${var.name_prefix}-parameter-store-backup"
-
-  tags = {
-    () = "True"
-    Environment                = var.environment_name
-    Application                = var.project_name
-    Name                       = "${var.name_prefix}"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-# The IAM role name is fixed as it is referenced in the KMS key policy in the backup destination account.
-resource "aws_iam_role" "iam_for_lambda_parameter_store_backup" {
-  name               = "parameter_store_lambda_encryption_role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
-}
-
 data "aws_iam_policy_document" "lambda_parameter_store_backup_permissions" {
+  count = var.backup_plan_config_parameter_store.enable ? 1 : 0
   version = "2012-10-17"
   statement {
     effect    = "Allow"
     actions   = [
       "iam:PassRole"
     ]
-    resources = [aws_iam_role.backup.arn]
+    resources = [aws_iam_role.iam_for_lambda_parameter_store_backup[0].arn]
   }
 
   statement {
@@ -51,29 +32,60 @@ data "aws_iam_policy_document" "lambda_parameter_store_backup_permissions" {
   }
 }
 
-resource "aws_iam_role_policy" "lambda_parameter_store_backup_iam_permissions" {
-  name   = "${var.name_prefix}_lambda_parameter_store_backup_iam_permissions_policy"
-  role   = aws_iam_role.iam_for_lambda_parameter_store_backup.id
-  policy = data.aws_iam_policy_document.lambda_post_build_version_permissions.json
-}
-
 data "archive_file" "lambda_parameter_store_backup_zip" {
+  count       = var.backup_plan_config_parameter_store.enable ? 1 : 0
   type        = "zip"
   source_dir  = "${path.module}/resources/parameter_store_backup/"
   output_path = "${path.module}/.terraform/archive_files/lambda_parameter_store_backup.zip"
 }
 
+# --- Resources ---
+
+resource "aws_s3_bucket" "parameter_store_backup_storage" {
+  count = var.backup_plan_config_parameter_store.enable ? 1 : 0
+  bucket = "${var.name_prefix}-parameter-store-backup"
+
+  tags = {
+    () = "True" # Assuming this is a placeholder/typo and should be a valid key
+    Environment                = var.environment_name
+    Application                = var.project_name
+    Name                       = "${var.name_prefix}"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# The IAM role name is fixed as it is referenced in the KMS key policy in the backup destination account.
+resource "aws_iam_role" "iam_for_lambda_parameter_store_backup" {
+  count = var.backup_plan_config_parameter_store.enable ? 1 : 0
+  name               = "parameter_store_lambda_encryption_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role[0].json # Access data source with index [0]
+}
+
+resource "aws_iam_role_policy" "lambda_parameter_store_backup_iam_permissions" {
+  count  = var.backup_plan_config_parameter_store.enable ? 1 : 0
+  name   = "${var.name_prefix}_lambda_parameter_store_backup_iam_permissions_policy"
+  role   = aws_iam_role.iam_for_lambda_parameter_store_backup[0].id # Access resource with index [0]
+  # NOTE: Original code referenced 'data.aws_iam_policy_document.lambda_post_build_version_permissions.json'
+  # which seems like a typo for 'data.aws_iam_policy_document.lambda_parameter_store_backup_permissions.json'.
+  # Using the corrected name with index [0].
+  policy = data.aws_iam_policy_document.lambda_parameter_store_backup_permissions[0].json
+}
+
 resource "aws_lambda_function" "lambda_parameter_store_backup" {
-  filename         = data.archive_file.lambda_parameter_store_backup_zip.output_path
-  source_code_hash = data.archive_file.lambda_parameter_store_backup_zip.output_base64sha256
+  count            = var.backup_plan_config_parameter_store.enable ? 1 : 0
+  filename         = data.archive_file.lambda_parameter_store_backup_zip[0].output_path
+  source_code_hash = data.archive_file.lambda_parameter_store_backup_zip[0].output_base64sha256
   function_name    = "${var.name_prefix}-parameter_store_backup"
-  role             = aws_iam_role.iam_for_lambda_parameter_store_backup.arn
+  role             = aws_iam_role.iam_for_lambda_parameter_store_backup[0].arn
   handler          = "parameter_store_backup.lambda_handler"
   runtime          = "python3.12"
   environment {
     variables = {
       KMS_KEY_ARN                 = var.parameter_store_kms_key_arn
-      PARAMETER_STORE_BUCKET_NAME = aws_s3_bucket.parameter_store_backup_storage.bucket
+      PARAMETER_STORE_BUCKET_NAME = aws_s3_bucket.parameter_store_backup_storage[0].bucket
       TAG_KEY                     = var.backup_plan_config_parameter_store.selection_tag
       TAG_VALUE                   = "true"
     }
@@ -81,6 +93,7 @@ resource "aws_lambda_function" "lambda_parameter_store_backup" {
 }
 
 resource "aws_cloudwatch_event_rule" "aws_backup_event_rule" {
+  count = var.backup_plan_config_parameter_store.enable ? 1 : 0
   name        = "${var.name_prefix}-parameter-store-backup-rule"
   description = "Triggers the ECR Backup lambda."
 
@@ -88,21 +101,27 @@ resource "aws_cloudwatch_event_rule" "aws_backup_event_rule" {
 }
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule      = aws_cloudwatch_event_rule.aws_backup_event_rule.name
-  arn       = aws_lambda_function.lambda_post_build_version.arn
+  count     = var.backup_plan_config_parameter_store.enable ? 1 : 0
+  rule      = aws_cloudwatch_event_rule.aws_backup_event_rule[0].name
+  # NOTE: Original code referenced 'aws_lambda_function.lambda_post_build_version.arn'
+  # which seems like a typo for 'aws_lambda_function.lambda_parameter_store_backup.arn'.
+  # Using the corrected name with index [0].
+  arn       = aws_lambda_function.lambda_parameter_store_backup[0].arn
   target_id = "${var.name_prefix}parameterStoreBackupLambdaTarget"
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
+  count         = var.backup_plan_config_parameter_store.enable ? 1 : 0
   statement_id  = "${var.name_prefix}AllowExecutionFromEventbridge"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_parameter_store_backup.function_name
+  function_name = aws_lambda_function.lambda_parameter_store_backup[0].function_name
   principal     = "events.amazonaws.com"
 
-  source_arn    = aws_cloudwatch_event_rule.aws_backup_event_rule.arn
+  source_arn    = aws_cloudwatch_event_rule.aws_backup_event_rule[0].arn
 }
 
 resource "aws_cloudwatch_log_group" "parameter_store_backup" {
+  count             = var.backup_plan_config_parameter_store.enable ? 1 : 0
   name              = "/aws/lambda/${var.name_prefix}-parameter-store-backup"
   retention_in_days = 30
 }
