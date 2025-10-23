1dace7e610afcc0f399cb65c771b3aaaa9a686a4
staplesn <nicholas.staples@solution-tribute.com>
2025-10-23 14:37:48 +0100

    Add missing parameter store aws_backup_plan.

--- modules/aws-backup-source/backup_plan.tf
+++ modules/aws-backup-source/backup_plan.tf
@@ -122,6 +122,40 @@ resource "aws_backup_plan" "aurora" {
   }
 }
 
+
+resource "aws_backup_plan" "default" {
+  count = var.backup_plan_config_parameter_store.enable ? 1 : 0
+  name = "${local.resource_name_prefix}-parameter-store-plan"
+
+  dynamic "rule" {
+    for_each = var.backup_plan_config_parameter_store.rules
+    content {
+      recovery_point_tags = {
+        backup_rule_name = rule.value.name
+      }
+      rule_name                = rule.value.name
+      target_vault_name        = aws_backup_vault.main.name
+      schedule                 = rule.value.schedule
+      completion_window        = rule.value.completion_window
+      enable_continuous_backup = rule.value.enable_continuous_backup != null ? rule.value.enable_continuous_backup : null
+      lifecycle {
+        delete_after       = rule.value.lifecycle.delete_after != null ? rule.value.lifecycle.delete_after : null
+        cold_storage_after = rule.value.lifecycle.cold_storage_after != null ? rule.value.lifecycle.cold_storage_after : null
+      }
+      dynamic "copy_action" {
+        for_each = var.backup_copy_vault_arn != "" && var.backup_copy_vault_account_id != "" && rule.value.copy_action != null ? rule.value.copy_action : {}
+        content {
+          lifecycle {
+            delete_after = copy_action.value
+          }
+          destination_vault_arn = var.backup_copy_vault_arn
+        }
+      }
+    }
+  }
+}
+
+
 resource "aws_backup_selection" "default" {
   iam_role_arn = aws_iam_role.backup.arn
   name         = "${local.resource_name_prefix}-selection"
