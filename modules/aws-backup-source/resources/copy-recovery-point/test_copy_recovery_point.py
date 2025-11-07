90ad5db3731618fc501ea0f9334e046da1d90150
Nick Miles <nick.miles5@nhs.net>
2025-11-07 13:06:51 +0000

    ENG-930 Copy recovery point lambda

---
+++ modules/aws-backup-source/resources/copy-recovery-point/test_copy_recovery_point.py
@@ -0,0 +1,21 @@
+import copy_recovery_point as crp
+from unittest.mock import patch, MagicMock
+
+class Ctx: aws_request_id = 'test-id'
+
+def test_start_copy_job_with_arn():
+    event = {"recovery_point_id": "arn:aws:backup:eu-west-2:123456789012:recovery-point:ABC"}
+    with patch.object(crp, 'backup_client') as mock_client:
+        mock_client.start_copy_job.return_value = {"CopyJobId": "job-1", "CreationDate": 0, "IsParent": False}
+        mock_client.describe_copy_job.return_value = {"CopyJob": {"CopyJobId": "job-1", "State": "CREATED"}}
+        resp = crp.lambda_handler(event, Ctx())
+        assert resp['statusCode'] == 200
+        assert resp['body']['copy_job']['copy_job_id'] == 'job-1'
+
+def test_poll_copy_job():
+    event = {"copy_job_id": "job-2"}
+    with patch.object(crp, 'backup_client') as mock_client:
+        mock_client.describe_copy_job.return_value = {"CopyJob": {"CopyJobId": "job-2", "State": "RUNNING"}}
+        resp = crp.lambda_handler(event, Ctx())
+        assert resp['statusCode'] == 200
+        assert resp['body']['state'] == 'RUNNING'
