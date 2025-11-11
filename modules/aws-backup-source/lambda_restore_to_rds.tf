d1e375ade37ee670c0d7065aab317c95c7fd8756
Nick Miles <nick.miles5@nhs.net>
2025-11-11 10:20:03 +0000

    ENG-932 Restore to RDS in source for testing
    Remove the thin wrapper
    
    Patching
    
    Fixes to deployment

---
+++ modules/aws-backup-source/lambda_restore_to_rds.tf
@@ -0,0 +1,82 @@
+data "archive_file" "lambda_restore_to_rds_zip" {
+  count       = var.lambda_restore_to_rds_enable ? 1 : 0
+  type        = "zip"
+  source_dir  = "${path.module}/resources/restore-to-rds/"
+  output_path = "${path.module}/.terraform/archive_files/lambda_restore_to_rds.zip"
+}
+
+resource "aws_iam_role" "iam_for_lambda_restore_to_rds" {
+  count = var.lambda_restore_to_rds_enable ? 1 : 0
+  name  = "${var.name_prefix}-lambda-restore-to-rds-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda_restore_to_rds" {
+  count = var.lambda_restore_to_rds_enable ? 1 : 0
+  name  = "${var.name_prefix}-lambda-restore-to-rds-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+        Effect   = "Allow"
+      },
+      {
+        Action = [
+          "backup:StartRestoreJob",
+          "backup:DescribeRestoreJob"
+        ]
+        Resource = "*"
+        Effect   = "Allow"
+      },
+      {
+        Action = "iam:PassRole"
+        Resource = aws_iam_role.iam_for_lambda_restore_to_rds[0].arn
+        Condition = {
+          StringEquals = {
+            "iam:PassedToService" : "backup.amazonaws.com"
+          }
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_restore_to_rds_policy_attach" {
+  count      = var.lambda_restore_to_rds_enable ? 1 : 0
+  role       = aws_iam_role.iam_for_lambda_restore_to_rds[0].name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda_restore_to_rds[0].arn
+}
+
+resource "aws_lambda_function" "lambda_restore_to_rds" {
+  count            = var.lambda_restore_to_rds_enable ? 1 : 0
+  function_name    = "${var.name_prefix}_lambda-restore-to-rds"
+  role             = aws_iam_role.iam_for_lambda_restore_to_rds[0].arn
+  handler          = "restore_to_rds.lambda_handler"
+  runtime          = "python3.12"
+  filename         = data.archive_file.lambda_restore_to_rds_zip[0].output_path
+  source_code_hash = data.archive_file.lambda_restore_to_rds_zip[0].output_base64sha256
+  timeout          = var.lambda_restore_to_rds_max_wait_minutes * 60
+
+  environment {
+    variables = {
+      POLL_INTERVAL_SECONDS = var.lambda_restore_to_rds_poll_interval_seconds
+      MAX_WAIT_MINUTES      = var.lambda_restore_to_rds_max_wait_minutes
+    }
+  }
+}
