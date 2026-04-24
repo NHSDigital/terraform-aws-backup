27808bd08fda6aaab80027b9b57ef6d7bba9c4bb
TurboNHS <turbo.fredriksson1@nhs.net>
2026-04-24 16:24:24 +0100

    Can't have a `null` value for the restore testing plan.
    If `awscc_backup_restore_testing_plan.backup_restore_testing_plan.recovery_point_selection.include_vaults`
    is `null`, it will cause the resource to be updated every time TF
    runs (if `var.enable_logically_air_gapped_vault` is set to `false`).
    
    Instead, set it to an empty array.

--- modules/aws-backup-source/backup_restore_testing.tf
+++ modules/aws-backup-source/backup_restore_testing.tf
@@ -4,7 +4,7 @@ resource "awscc_backup_restore_testing_plan" "backup_restore_testing_plan" {
   start_window_hours        = var.restore_testing_plan_start_window
   recovery_point_selection = {
     algorithm             = var.restore_testing_plan_algorithm
-    include_vaults        = [aws_backup_vault.main.arn, var.enable_logically_air_gapped_vault ? aws_backup_logically_air_gapped_vault.main[0].arn : null]
+    include_vaults        = concat([aws_backup_vault.main.arn], (var.enable_logically_air_gapped_vault ? [aws_backup_logically_air_gapped_vault.main[0].arn] : []))
     recovery_point_types  = var.restore_testing_plan_recovery_point_types
     selection_window_days = var.restore_testing_plan_selection_window_days
   }
