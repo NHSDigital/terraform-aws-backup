a08d6bd8bd0302d959415401174f0d5a1e4c1958
TurboNHS <turbo.fredriksson1@nhs.net>
2026-04-27 11:18:47 +0100

    Run code through `terraform fmt`.

--- modules/aws-backup-source/backup_plan.tf
+++ modules/aws-backup-source/backup_plan.tf
@@ -173,10 +173,10 @@ resource "aws_backup_selection" "default" {
   condition {
     dynamic "string_equals" {
       for_each = concat(local.selection_tags_null_checked, [
-	{
-	  "key": var.backup_plan_config.selection_tag,
-	  "value": var.backup_plan_config.selection_tag_value != null ? var.backup_plan_config.selection_tag_value : "True"
-	}
+        {
+          "key" : var.backup_plan_config.selection_tag,
+          "value" : var.backup_plan_config.selection_tag_value != null ? var.backup_plan_config.selection_tag_value : "True"
+        }
       ])
       content {
         key   = (try(string_equals.value.key, null) == null) ? null : "aws:ResourceTag/${string_equals.value.key}"
@@ -200,10 +200,10 @@ resource "aws_backup_selection" "dynamodb" {
   condition {
     dynamic "string_equals" {
       for_each = concat(local.selection_tags_dynamodb_null_checked, [
-	{
-	  "key": var.backup_plan_config_dynamodb.selection_tag,
-	  "value": var.backup_plan_config_dynamodb.selection_tag_value != null ? var.backup_plan_config_dynamodb.selection_tag_value : "True"
-	}
+        {
+          "key" : var.backup_plan_config_dynamodb.selection_tag,
+          "value" : var.backup_plan_config_dynamodb.selection_tag_value != null ? var.backup_plan_config_dynamodb.selection_tag_value : "True"
+        }
       ])
       content {
         key   = (try(string_equals.value.key, null) == null) ? null : "aws:ResourceTag/${string_equals.value.key}"
@@ -227,10 +227,10 @@ resource "aws_backup_selection" "ebsvol" {
   condition {
     dynamic "string_equals" {
       for_each = concat(local.selection_tags_ebsvol_null_checked, [
-	{
-	  "key": var.backup_plan_config_ebsvol.selection_tag,
-	  "value": var.backup_plan_config_ebsvol.selection_tag_value != null ? var.backup_plan_config_ebsvol.selection_tag_value : "True"
-	}
+        {
+          "key" : var.backup_plan_config_ebsvol.selection_tag,
+          "value" : var.backup_plan_config_ebsvol.selection_tag_value != null ? var.backup_plan_config_ebsvol.selection_tag_value : "True"
+        }
       ])
       content {
         key   = (try(string_equals.value.key, null) == null) ? null : "aws:ResourceTag/${string_equals.value.key}"
@@ -254,10 +254,10 @@ resource "aws_backup_selection" "aurora" {
   condition {
     dynamic "string_equals" {
       for_each = concat(local.selection_tags_aurora_null_checked, [
-	{
-	  "key": var.backup_plan_config_aurora.selection_tag,
-	  "value": var.backup_plan_config_aurora.selection_tag_value != null ? var.backup_plan_config_aurora.selection_tag_value : "True"
-	}
+        {
+          "key" : var.backup_plan_config_aurora.selection_tag,
+          "value" : var.backup_plan_config_aurora.selection_tag_value != null ? var.backup_plan_config_aurora.selection_tag_value : "True"
+        }
       ])
       content {
         key   = (try(string_equals.value.key, null) == null) ? null : "aws:ResourceTag/${string_equals.value.key}"
@@ -281,10 +281,10 @@ resource "aws_backup_selection" "parameter_store" {
   condition {
     dynamic "string_equals" {
       for_each = concat(local.selection_tags_parameter_store_null_checked, [
-	{
-	  "key": var.backup_plan_config_parameter_store.selection_tag,
-	  "value": var.backup_plan_config_parameter_store.selection_tag_value != null ? var.backup_plan_config_parameter_store.selection_tag_value : "True"
-	}
+        {
+          "key" : var.backup_plan_config_parameter_store.selection_tag,
+          "value" : var.backup_plan_config_parameter_store.selection_tag_value != null ? var.backup_plan_config_parameter_store.selection_tag_value : "True"
+        }
       ])
       content {
         key   = (try(string_equals.value.key, null) == null) ? null : "aws:ResourceTag/${string_equals.value.key}"
