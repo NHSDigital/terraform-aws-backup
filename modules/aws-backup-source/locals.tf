03a39d4d45774e28dbe5458038f3816e8e114899
susana-zhou1 <susana.zhou1@nhs.net>
2026-08-06 11:07:56 +0100

    ENG-1371 add tag value null check

--- modules/aws-backup-source/locals.tf
+++ modules/aws-backup-source/locals.tf
@@ -2,9 +2,13 @@ locals {
   resource_name_prefix                             = var.name_prefix != null ? var.name_prefix : "${data.aws_region.current.id}-${data.aws_caller_identity.current.account_id}-backup"
   selection_tag_value_null_checked                 = (var.backup_plan_config.selection_tag_value == null) ? "True" : var.backup_plan_config.selection_tag_value
   selection_tags_null_checked                      = (var.backup_plan_config.selection_tags == null) ? [] : var.backup_plan_config.selection_tags
+  selection_tag_value_dynamodb_null_checked       = (var.backup_plan_config_dynamodb.selection_tag_value == null) ? "True" : var.backup_plan_config_dynamodb.selection_tag_value
   selection_tags_dynamodb_null_checked             = (var.backup_plan_config_dynamodb.selection_tags == null) ? [] : var.backup_plan_config_dynamodb.selection_tags
+  selection_tag_value_ebsvol_null_checked         = (var.backup_plan_config_ebsvol.selection_tag_value == null) ? "True" : var.backup_plan_config_ebsvol.selection_tag_value
   selection_tags_ebsvol_null_checked               = (var.backup_plan_config_ebsvol.selection_tags == null) ? [] : var.backup_plan_config_ebsvol.selection_tags
+  selection_tag_value_parameter_store_null_checked = (var.backup_plan_config_parameter_store.selection_tag_value == null) ? "True" : var.backup_plan_config_parameter_store.selection_tag_value
   selection_tags_parameter_store_null_checked      = (var.backup_plan_config_parameter_store.selection_tags == null) ? [] : var.backup_plan_config_parameter_store.selection_tags
+  selection_tag_value_aurora_null_checked          = (var.backup_plan_config_aurora.selection_tag_value == null) ? "True" : var.backup_plan_config_aurora.selection_tag_value
   selection_tags_aurora_null_checked               = (var.backup_plan_config_aurora.selection_tags == null) ? [] : var.backup_plan_config_aurora.selection_tags
 
   framework_arn_list = flatten(concat(
